8e52723234c2f1f7fad9b1222c3a3e3e7af6a05d
amardeep <amars123456@gmail.com>
2016-03-08 20:44:09 +0530

    done

---
+++ array_compare.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main () {
+
+int n,flag,arr[20],arr2[20];
+
+printf("enter the number of elements\n");
+scanf("%d",&n);
+
+for(int i =0;i<n;i++){
+	printf("arr[%d]=\n",arr[i]);
+	scanf("%d",&arr[i]);
+}
+
+for(int i =0;i<n;i++){
+	printf("arr[%d]=\n",arr2[i]);
+	scanf("%d",&arr2[i]);
+}
+
+for(int i=0;i<n;i++){
+	if(arr[i]==arr2[i]){
+		flag=1;
+	}
+	else {
+		flag=0;
+		break;
+	}
+}
+
+if (flag ==1){
+	printf("array is equal\n");
+}
+
+else {
+	printf("array is not equal\n");
+}
+
+return 0;
+}
