04f7b4b0cbfa0e01e1f05e54f6cfd73720980d39
amardeep <amars123456@gmail.com>
2016-03-09 20:28:13 +0530

    done

---
+++ mutiplication.c
@@ -0,0 +1,18 @@
+/*program to compute multiplication table of user defined*/
+
+int main () {
+
+	int n;
+	printf("enter the number for table \n");
+	scanf("%d",&n);
+
+	for(int i =1;i<=10;i++){
+		
+		
+	printf("%d * %d =%d\n",n,i,n*i);
+
+
+	}
+
+	return 0;
+} 
